2108a0d0583b6387828d74a98e4ee99343a76ed9
Bryan Ho <bryannho@usc.edu>
2023-10-05 11:50:50 -0700

    enable reports

--- ui-tests/playwright.config.js
+++ ui-tests/playwright.config.js
@@ -5,6 +5,7 @@ const baseConfig = require('@jupyterlab/galata/lib/playwright-config');
 
 module.exports = {
   ...baseConfig,
+  reporter: [['html', { open: 'never' }]],
   retries: process.env.CI ? 2 : 0, // set to 2 when running on CI
   use: {
     trace: 'on-first-retry', // record traces on first retry of each test
